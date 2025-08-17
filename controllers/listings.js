6d58303340f4b35d5f055fabd4a49bf64d02c469
Manideep0007 <mukkamanideep38@gmail.com>
2025-08-17 20:02:49 +0530

    fixed the undefined path error in edit page

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,78 +1,84 @@
 const listing = require("../models/listings.js");
 
-
-
-module.exports.index=async(req,res)=>{
-    let alllistings=await listing.find({});
-    res.render("listings/index.ejs" ,{alllistings});
-}
-
-module.exports.renderNew=(req,res)=>{
-    res.render("listings/new.ejs"); //is loggedin middleware;
+module.exports.index = async (req, res) => {
+  let alllistings = await listing.find({});
+  res.render("listings/index.ejs", { alllistings });
 };
 
-module.exports.show=async(req,res)=>{
-    let {id}=req.params;
-    let showData= await listing.findById(id).populate({path:"review",populate:{path:"author"}}).populate("owner");
-    if(!showData){
-    req.flash("failure","The listing yor requested is not available");  
-    return res.redirect("/listings")
-    }
-    res.render("listings/show.ejs",{showData});
+module.exports.renderNew = (req, res) => {
+  res.render("listings/new.ejs"); //is loggedin middleware;
 };
 
-module.exports.postNew=async(req,res,next)=>{
-    const url=req.file.path;
-    const filename=req.file.filename;
-    let listingData = req.body.listing;
-    listingData.owner=req.user._id;
-    listingData.image={url,filename};
-    await listing.insertOne(listingData);
-    req.flash("success","new listing added successfully");  
-    res.redirect("/listings");
+module.exports.show = async (req, res) => {
+  let { id } = req.params;
+  let showData = await listing
+    .findById(id)
+    .populate({ path: "review", populate: { path: "author" } })
+    .populate("owner");
+  if (!showData) {
+    req.flash("failure", "The listing yor requested is not available");
+    return res.redirect("/listings");
+  }
+  res.render("listings/show.ejs", { showData });
 };
 
-module.exports.delete=async(req,res)=>{
-    let {id}=req.params;
-    await listing.findByIdAndDelete(id);
-    req.flash("success","listing deleted successfully");  
-    res.redirect("/listings");
+module.exports.postNew = async (req, res, next) => {
+  if (!req.file) {
+    req.flash("failure", "Please upload an image");
+    return res.redirect("/listings/new");
+  }
+  const url = req.file.path;
+  const filename = req.file.filename;
+  let listingData = req.body.listing;
+  listingData.owner = req.user._id;
+  listingData.image = { url, filename };
+  let newListing = new listing(listingData);
+  await newListing.save();
+  req.flash("success", "new listing added successfully");
+  res.redirect("/listings");
 };
 
-module.exports.renderEdit=async(req,res,next)=>{
-    let {id}=req.params;
-    let showData= await listing.findById(id);
-    if(!showData){
-    req.flash("failure","The listing yor requested is not available");  
-    return res.redirect("/listings")
-    }
-    let originalimageurl=showData.image.url;
-    originalimageurl=originalimageurl.replace("/upload","/upload/h_250,w_250");
-    req.flash("success","edited successfully");
-    res.render("listings/edit.ejs",{showData,originalimageurl});
-    // try catch block catches the async errors
-    // try{
-    // let {id}=req.params;
-    // let showData= await listing.findById(id);
-    // res.render("listings/edit.ejs",{showData});
-    // }
-    // catch(err){
-    //     next(err); // calls our error middleware handler i.e router.use
-    // }
+module.exports.delete = async (req, res) => {
+  let { id } = req.params;
+  await listing.findByIdAndDelete(id);
+  req.flash("success", "listing deleted successfully");
+  res.redirect("/listings");
 };
 
-module.exports.postEdit=async(req,res)=>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send valid data");
-    // } instead of each if for a model we use JOI
-    let {id}=req.params;
-    let listings=await listing.findByIdAndUpdate(id,{...req.body.listing});
-    if(typeof(req.file)!=undefined){
-    const url=req.file.path;
-    const filename=req.file.filename;
-    listings.image={url,filename};
+module.exports.renderEdit = async (req, res, next) => {
+  let { id } = req.params;
+  let showData = await listing.findById(id);
+  if (!showData) {
+    req.flash("failure", "The listing yor requested is not available");
+    return res.redirect("/listings");
+  }
+  let originalimageurl = showData.image.url;
+  originalimageurl = originalimageurl.replace("/upload", "/upload/h_250,w_250");
+  req.flash("success", "edited successfully");
+  res.render("listings/edit.ejs", { showData, originalimageurl });
+  // try catch block catches the async errors
+  // try{
+  // let {id}=req.params;
+  // let showData= await listing.findById(id);
+  // res.render("listings/edit.ejs",{showData});
+  // }
+  // catch(err){
+  //     next(err); // calls our error middleware handler i.e router.use
+  // }
+};
+
+module.exports.postEdit = async (req, res) => {
+  // if(!req.body.listing){
+  //     throw new ExpressError(400,"Send valid data");
+  // } instead of each if for a model we use JOI
+  let { id } = req.params;
+  let listings = await listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if (req.file) {
+    const url = req.file.path;
+    const filename = req.file.filename;
+    listings.image = { url, filename };
     await listings.save();
-    }
-    req.flash("success","Updated successfully");    
-    res.redirect(`/listings/${id}`);
+  }
+  req.flash("success", "Updated successfully");
+  return res.redirect(`/listings/${id}`);
 };
